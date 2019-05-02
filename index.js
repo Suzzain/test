283d9d4b1b3779fa49bdaa782a464d5cc0bf119e
Suzzain <gurungsujen@gmail.com>
2019-05-02 08:26:57 +0545

    application server initialized

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("Index is running");
-console.log("index 2nd");
+// console.log("Index is running");
+// console.log("index 2nd");
+
+var express = require('express');
+var application = new express();
+
+application.listen(3001);
